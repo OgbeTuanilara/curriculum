8a2d11878ac1be017ef1e9e2573037e90fac14d9
Ogbe Tua <you@example.com>
2019-07-14 04:30:00 -0400

    listo todo falta es el responsivo desing

---
+++ js/Revel.js
@@ -0,0 +1,15 @@
+window.sr = ScrollReveal();
+//las fotos de index.html
+sr.reveal('#ozain', {
+        duration: 3000,
+        origin: 'left',
+});
+sr.reveal('#yemaya', {
+    duration: 3000,
+    origin: 'right',
+});
+sr.reveal('#obba', {
+    duration: 3000,
+    origin: 'top',
+    distance: '300px',
+});
