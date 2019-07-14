7c1b5ce47ddf9de09a28c4ab93a9b7d319d385cb
Ogbe Tua <you@example.com>
2019-07-14 14:19:23 -0400

    metiendo efecto

--- js/Revel.js
+++ js/Revel.js
@@ -1,15 +1,110 @@
+$(document).ready(function(){
 window.sr = ScrollReveal();
 //las fotos de index.html
-sr.reveal('#ozain', {
-        duration: 3000,
-        origin: 'left',
+sr.reveal('.jumbotron', {
+        duration: 2000,
+        origin: 'top',
+        distance: "300px"
 });
-sr.reveal('#yemaya', {
+sr.reveal('.text', {
+    duration: 2000,
+    origin: 'right',
+    delay: 1000
+});
+sr.reveal('.text1', {
+    duration: 2000,
+    origin: 'right',
+    delay: 1500
+});
+sr.reveal('.navegador', {
+    duration: 2000,
+    origin: 'right',
+    distance: "300px"
+});
+sr.reveal('.text2', {
+    duration: 2000,
+    origin: 'right',
+    delay: 1000
+});
+sr.reveal('.text3', {
+    duration: 2000,
+    origin: 'right',
+    delay: 1500
+});
+sr.reveal('.estudios-realizados', {
+    duration: 2500,
+    origin: 'left',
+    distance: "300px"
+});
+sr.reveal('.text4', {
+    duration: 2500,
+    origin: 'right',
+    delay: 2500
+});
+sr.reveal('.text5', {
+    duration: 2500,
+    origin: 'right',
+    delay: 3000
+});
+sr.reveal('.text6', {
     duration: 3000,
     origin: 'right',
+    delay: 3000
 });
-sr.reveal('#obba', {
+sr.reveal('.text7', {
     duration: 3000,
     origin: 'top',
-    distance: '300px',
+    delay: 3000
+});
+sr.reveal('.text8', {
+    duration: 3000,
+    origin: 'top',
+    delay: 3500
+});
+sr.reveal('.text9', {
+    duration: 3000,
+    origin: 'top',
+    delay: 4000
+});
+sr.reveal('.text10', {
+    duration: 3000,
+    origin: 'top',
+    delay: 4500
+});
+sr.reveal('.text11', {
+    duration: 3000,
+    origin: 'top',
+    delay: 5000
+});
+sr.reveal('.text12', {
+    duration: 3000,
+    origin: 'top',
+    delay: 5500
+});
+sr.reveal('.text13', {
+    duration: 3000,
+    origin: 'top',
+    delay: 6000
+});
+sr.reveal('.text14', {
+    duration: 3000,
+    origin: 'top',
+    delay: 6500
+});
+sr.reveal('.text15', {
+    duration: 3000,
+    origin: 'top',
+    delay: 7000
+});
+sr.reveal('.text16', {
+    duration: 3000,
+    origin: 'top',
+    delay: 8000
+});
+
+
+
+
+
+
 });
