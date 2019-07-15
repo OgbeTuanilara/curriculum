a82700ab8ccdc4426f8b59d03397a7b5804e804c
Ogbe Tua <you@example.com>
2019-07-14 20:11:08 -0400

    actualizando

--- js/Revel.js
+++ js/Revel.js
@@ -29,7 +29,7 @@ sr.reveal('.text2', {
 sr.reveal('.text3', {
     duration: 2000,
     origin: 'right',
-    delay: 1500
+    delay: 1100
 });
 sr.reveal('.estudios-realizados', {
     duration: 2000,
@@ -39,92 +39,92 @@ sr.reveal('.estudios-realizados', {
 sr.reveal('.text4', {
     duration: 2000,
     origin: 'right',
-    delay: 1000
+    delay: 1200
 });
 sr.reveal('.text5', {
     duration: 2000,
     origin: 'right',
-    delay: 1000
+    delay: 1300
 });
 sr.reveal('.text6', {
     duration: 2000,
     origin: 'right',
-    delay: 1500
+    delay: 1400
 });
 sr.reveal('.text7', {
     duration: 2000,
     origin: 'top',
-    delay: 2300
+    delay: 1500
 });
 sr.reveal('.text8', {
     duration: 2200,
     origin: 'top',
-    delay: 2400
+    delay: 1600
 });
 sr.reveal('.text9', {
     duration: 2000,
     origin: 'top',
-    delay: 2500
+    delay: 1700
 });
 sr.reveal('.text10', {
     duration: 2000,
     origin: 'top',
-    delay: 2600
+    delay: 1800
 });
 sr.reveal('.text11', {
     duration: 2000,
     origin: 'top',
-    delay: 2700
+    delay: 1900
 });
 sr.reveal('.text12', {
     duration: 2000,
     origin: 'top',
-    delay: 2800
+    delay: 2000
 });
 sr.reveal('.text13', {
     duration: 2000,
     origin: 'top',
-    delay: 2900
+    delay: 2100
 });
 sr.reveal('.text14', {
     duration: 2000,
     origin: 'top',
-    delay: 3000
+    delay: 2200
 });
 sr.reveal('.text15', {
     duration: 2000,
     origin: 'top',
-    delay: 3800
+    delay: 2300
 });
 sr.reveal('.text16', {
     duration: 2000,
     origin: 'top',
-    delay: 4000
+    delay: 2400
 });
 sr.reveal('.dato', {
     duration: 2000,
     origin: 'top',
-    delay: 1000
+    delay: 1500
 });
 sr.reveal('.dato1', {
     duration: 2000,
     origin: 'top',
-    delay: 1200
+    delay: 1600
 });
 sr.reveal('.dato2', {
     duration: 2000,
     origin: 'top',
-    delay: 1400
+    delay: 1700
 });
 sr.reveal('.dato3', {
     duration: 2000,
     origin: 'top',
-    delay: 1600
+    delay: 1800
 });
 sr.reveal('.dato4', {
     duration: 2000,
     origin: 'top',
-    delay: 1800
+    delay: 1900
 });
 sr.reveal('.dato5', {
     duration: 2000,
@@ -134,57 +134,57 @@ sr.reveal('.dato5', {
 sr.reveal('.dato6', {
     duration: 2000,
     origin: 'top',
-    delay: 2200
+    delay: 2100
 });
 sr.reveal('.dato7', {
     duration: 2000,
     origin: 'top',
-    delay: 2400
+    delay: 2200
 });
 sr.reveal('.dato8', {
     duration: 2000,
     origin: 'top',
-    delay: 2600
+    delay: 2300
 });
 sr.reveal('.dato9', {
     duration: 2000,
     origin: 'top',
-    delay: 2800
+    delay: 2400
 });
 sr.reveal('.dato10', {
     duration: 2000,
     origin: 'top',
-    delay: 3000
+    delay: 2500
 });
 sr.reveal('.dato11', {
     duration: 2000,
     origin: 'top',
-    delay: 3100
+    delay: 2600
 });
 sr.reveal('.dato12', {
     duration: 2000,
     origin: 'top',
-    delay: 3200
+    delay: 2700
 });
 sr.reveal('.dato13', {
     duration: 2000,
     origin: 'top',
-    delay: 3300
+    delay: 2800
 });
 sr.reveal('.dato14', {
     duration: 2000,
     origin: 'top',
-    delay: 3400
+    delay: 2900
 });
 sr.reveal('.dato15', {
     duration: 2000,
     origin: 'top',
-    delay: 3500
+    delay: 3000
 });
 sr.reveal('.pies-pagina',{
     duration: 2000,
     origin: 'top',
     distance: '300px',
-    delay: 1000
+    delay: 2000
 });
 });
